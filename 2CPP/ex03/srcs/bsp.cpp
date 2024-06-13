b53e4b753d784d350022db13d99c8f89c1b72697
Alec Croue <alec.croue@gmail.com>
2024-06-13 17:00:33 +0200

    idk

--- 2CPP/ex03/srcs/bsp.cpp
+++ 2CPP/ex03/srcs/bsp.cpp
@@ -1,23 +1,42 @@
 #include "Fixed.hpp"
 #include "Point.hpp"
 #include <ostream>
+#include <math.h>
+
 
 // i(j − k) + j(k − i) + k(i − j)
 Fixed	area(const Point i, const Point j, const Point k)
 {
-	Fixed x_one = i.getX().toFloat() * (j.getY().toFloat() - k.getY().toFloat());
-	Fixed x_two = j.getX().toFloat() * (k.getY().toFloat() - i.getY().toFloat());
-	Fixed x_three = k.getX().toFloat() * (i.getY().toFloat() - j.getY().toFloat());
+	const float	x_one = roundf((i.getX().toFloat() * (j.getY().toFloat() - k.getY().toFloat())) * 10);
+	const float	x_two = roundf((j.getX().toFloat() * (k.getY().toFloat() - i.getY().toFloat())) * 10);
+	const float	x_three = roundf((k.getX().toFloat() * (i.getY().toFloat() - j.getY().toFloat())) * 10);
 
-	Fixed area = (x_one + x_two + x_three) / 2;
+	const float	area = roundf((x_one + x_two + x_three) / 2);
 	return ((area > 0) ? area : area * -1);
 }
 
 bool	checkSides(Point const e, Point const f, Point const g)
 {
-	if (e.getX().toFloat() == f.getX().toFloat() == g.getX().toFloat())
+	const float	ex = roundf(e.getX().toFloat() * 100);
+	const float	ey = roundf(e.getY().toFloat() * 100);
+	const float	fx = roundf(f.getX().toFloat() * 100);
+	const float	fy = roundf(f.getY().toFloat() * 100);
+	const float	gx = roundf(g.getX().toFloat() * 100);
+	const float	gy = roundf(g.getY().toFloat() * 100);
+
+	// if (ey == fy)
+	// 	std::cout << ey << " eq " << fy << std::endl;
+	// if (ey == gy)
+	// 	std::cout << ey << " eq " << gy << std::endl;
+	// if (fy == gy)
+	// 	std::cout << fy << " eq " << gy << std::endl;
+
+	// std::cout << ex << " " << fx << " " << gx << std::endl;
+	// std::cout << ey << " " << fy << " " << gy << std::endl << std::endl;
+
+	if (ex == gx && fx == gx)
 		return true;
-	else if (e.getY().toFloat() == f.getY().toFloat() == g.getY().toFloat())
+	else if (ey == gy && fy == gy)
 		return true;
 	else
 		return false;
@@ -25,9 +44,13 @@ bool	checkSides(Point const e, Point const f, Point const g)
 
 bool	checkPoints(Point const z, Point const w)
 {
-	if (z.getX().toFloat() == w.getX().toFloat())
+	const float	zx = z.getX().toFloat();
+	const float	zy = z.getY().toFloat();
+	const float	wx = w.getX().toFloat();
+	const float	wy = w.getY().toFloat();
+	if (zx == wx)
 	{
-		if (z.getY().toFloat() == w.getY().toFloat())
+		if (zy == wy)
 			return true;
 	}
 	return false;
@@ -36,22 +59,22 @@ bool	checkPoints(Point const z, Point const w)
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	if (checkSides(a, b, point) || checkSides(a, c, point) || checkSides(b, c, point))
+	{
+		std::cout << "Point is on the edge of the triangle" << std::endl;
 		return false;
+	}
 	if (checkPoints(a, point) || checkPoints(b, point) || checkPoints(c, point))
+	{
+		std::cout << "Point is on the apex of the triangle" << std::endl;
 		return false;
-	// std::cout << area(a, b, c) << std::endl;
-	// std::cout << area(a, b, point) << std::endl;
-	// std::cout << area(a, c, point) << std::endl;
-	// std::cout << area(b, c, point) << std::endl;
+	}
+	std::cout << area(a, b, c) << std::endl;
+	std::cout << area(a, b, point) << std::endl;
+	std::cout << area(a, c, point) << std::endl;
+	std::cout << area(b, c, point) << std::endl;
 	if (area(a, b, c) != area(a, b, point) + area(b, c, point) + area(a, c, point))
 	{
-		// std::cout << "Point is not inside triangle" << std::endl;
 		return (0);
 	}
-	// std::cout << "Point is inside triangle" << std::endl;
-	// (void)a;
-	// (void)b;
-	// (void)c;
-	// (void)point;
 	return (1);
 }
--- 2CPP/ex03/srcs/main.cpp
+++ 2CPP/ex03/srcs/main.cpp
@@ -3,17 +3,6 @@
 int main()
 {
 	{
-	/*own*/
-		Point a(0, 0);
-		Point b(2, 4);
-		Point c(3, 1);
-		Point to_check(2, 2);
-
-		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
-		else
-		 std::cout <<"Not Inside" << std::endl;
-	}{
 	/*edge*/
 		Point a(0, 0);
 		Point e(20, 0);
@@ -21,9 +10,9 @@ int main()
 		Point g(10, 0);
 
 		if (bsp(a, e, f, g))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*point of the triangle*/
@@ -33,9 +22,9 @@ int main()
 		Point to_check(10, 30);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*edge with floats*/
@@ -45,9 +34,9 @@ int main()
 		Point to_check(13.8, 15.6);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*point with floats*/
@@ -57,9 +46,9 @@ int main()
 		Point to_check(15.78, 42.42);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*inside*/
@@ -69,9 +58,9 @@ int main()
 		Point to_check(15.78, 41.42);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*inside*/
@@ -81,9 +70,9 @@ int main()
 		Point to_check(15.78, 39.42);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*inside*/
@@ -93,21 +82,21 @@ int main()
 		Point to_check(12.28, 16.6);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	{
 	/*inside*/
-		Point a(10.2, 15.6);
-		Point b(20.55, 15.6);
-		Point c(15.78, 42.42);
-		Point to_check(19.54, 16.7);
+		Point a(0, 0);
+		Point b(2, 4);
+		Point c(3, 1);
+		Point to_check(2, 2);
 
 		if (bsp(a, b, c, to_check))
-		 std::cout <<"Inside" << std::endl;
+		 std::cout <<"Inside" << std::endl << std::endl;
 		else
-		 std::cout <<"Not Inside" << std::endl;
+		 std::cout <<"Not Inside" << std::endl << std::endl;
 	}
 	return 0;
 }
